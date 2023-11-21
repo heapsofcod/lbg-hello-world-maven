096a0469915b30bdaac4df3eb38e9dd70aaf77f6
Rhian <heapster-100@hotmail.com>
2023-11-21 14:30:33 +0000

    fixed the test that was broken. what a fool i was

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -22,7 +22,7 @@ public class AppTest
     public void test_shouldAnswerWithTen()
     {
         int answer = 10;
-        assertEquals(9, answer );
+        assertEquals(10, answer );
     }
 
     @Test
